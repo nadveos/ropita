c3fa3c52b82c9213603d22c06b285f7a71f8fd96
Gustavo Flores <guta.flores@gmail.com>
2023-11-11 17:45:18 -0300

    removing package for cached images

--- lib/presentations/widgets/man_products.dart
+++ lib/presentations/widgets/man_products.dart
@@ -1,6 +1,4 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
@@ -18,8 +16,7 @@ class _ManProductsState extends State<ManProducts> {
   ScrollController controller = ScrollController();
 
   List<SimpleProduct> ropa = [];
-static final _cacheManager = CacheManager(Config('onlyMans',
-      stalePeriod: const Duration(days: 15), maxNrOfCacheObjects: 50));
+
   @override
   Widget build(BuildContext context) {
     final data = context.watch<DataProvider>();
@@ -49,34 +46,21 @@ static final _cacheManager = CacheManager(Config('onlyMans',
                       fit: StackFit.loose,
                       alignment: Alignment.center,
                       children: [
-                        InkWell(
-                          onTap: () {
-                            Navigator.pushNamed(context, '/details', arguments: ropa[index]);
-                          },
-                          child: CachedNetworkImage(
-                            cacheManager: _cacheManager,
-                            imageUrl: ropa[index].getImageUrl(),
-                            key: UniqueKey(),
-                            width: double.infinity,
-                            height: double.infinity,
-                            fit: BoxFit.cover,
-                            
+                        
+                        Ink.image(
+                          width: double.infinity,
+                          height: double.infinity,
+                          fit: BoxFit.cover,
+                          image: NetworkImage(
+                            ropa[index].getImageUrl(),
+                          ),
+                          child: InkWell(
+                            onTap: () {
+                              Navigator.pushNamed(context, '/details',
+                                  arguments: ropa[index]);
+                            },
                           ),
                         ),
-                        // Ink.image(
-                        //   width: double.infinity,
-                        //   height: double.infinity,
-                        //   fit: BoxFit.cover,
-                        //   image: NetworkImage(
-                        //     ropa[index].getImageUrl(),
-                        //   ),
-                        //   child: InkWell(
-                        //     onTap: () {
-                        //       Navigator.pushNamed(context, '/details',
-                        //           arguments: ropa[index]);
-                        //     },
-                        //   ),
-                        // ),
                         Text(
                           ropa[index].name,
                           softWrap: true,
